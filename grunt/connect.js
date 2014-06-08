d1f96a1d1780017965bdae37781bf63495ee51a3
bouzuya <bouzuya@gmail.com>
2014-06-08 17:08:10 +0900

    fix grunt-contrib-connect setting

--- grunt/connect.js
+++ grunt/connect.js
@@ -4,7 +4,21 @@ module.exports = {
       port: 9000,
       base: ['src', 'build'],
       livereload: 35729,
-      open: true
+      open: true,
+      middleware: function(connect, options, middlewares) {
+        return [
+          function(req, res, next) {
+            var parse = require('url').parse;
+            var extname = require('path').extname;
+            var url = parse(req.url);
+            var file = url.pathname.split('/').splice(-1)[0];
+            if (!extname(file) && req.url.indexOf('/') === 0) {
+              req.url = req.url.replace('/', '#');
+            }
+            next();
+          }
+        ].concat(middlewares);
+      }
     }
   },
   build: {
