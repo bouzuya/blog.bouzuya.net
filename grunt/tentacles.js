42e8def6e155aaedddecdc720427034095c848e0
bouzuya <m@bouzuya.net>
2014-12-21 00:58:49 +0900

    update tentacles config

--- grunt/tentacles.js
+++ grunt/tentacles.js
@@ -2,9 +2,7 @@ module.exports = {
   upload: {
     options: {
       bucketName: 'blog.bouzuya.net',
-      endpoint: 'https://s3-ap-northeast-1.amazonaws.com',
-      s3BucketEndpoint: true,
-      s3ForcePathStyle: true
+      sslEnabled: false
     },
     files: [
       { cwd: 'build', src: '**', filter: 'isFile' }
