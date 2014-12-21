d95d49e2c01f155c1d7f75987d6e56c0696a92a1
bouzuya <m@bouzuya.net>
2014-12-21 09:49:54 +0900

    CERT_UNTRUSTED

--- grunt/tentacles.js
+++ grunt/tentacles.js
@@ -1,8 +1,7 @@
 module.exports = {
   upload: {
     options: {
-      bucketName: 'blog.bouzuya.net',
-      sslEnabled: false
+      bucketName: 'blog.bouzuya.net'
     },
     files: [
       { cwd: 'build', src: '**', filter: 'isFile' }
